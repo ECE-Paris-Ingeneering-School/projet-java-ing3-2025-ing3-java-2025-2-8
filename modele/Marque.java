b9e9f4418ab3dbf148a819230f8228d4c614fc2f
chris <chris.najjar@edu.ece.fr>
2025-04-14 17:43:52 +0200

    creation de class dans le package model

---
+++ modele/Marque.java
@@ -0,0 +1,33 @@
+
+package modele;
+
+public class Marque {
+    private int idMarque;
+    private String nom;
+
+    public Marque(int idMarque, String nom) {
+        this.idMarque = idMarque;
+        this.nom = nom;
+    }
+
+    public int getIdMarque() {
+        return idMarque;
+    }
+
+    public void setIdMarque(int idMarque) {
+        this.idMarque = idMarque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
