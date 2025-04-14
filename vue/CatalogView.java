6c41173a97abadfac8718c3f26372fd681917aae
florentmnr <126877916+florentmnr@users.noreply.github.com>
2025-04-14 14:32:32 +0200

    creation vue login/register + debut catalog : pas encore branché bdd

--- vue/CatalogView.java
+++ vue/CatalogView.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+//
 /**
  * CatalogView - Interface graphique pour l'affichage du catalogue des articles
  * Cette classe permet au client de visualiser, rechercher et filtrer les articles disponibles
