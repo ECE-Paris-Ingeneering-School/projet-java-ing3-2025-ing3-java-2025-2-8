7797e9863ea8ff6bfadeb89042905d75bd8911e6
AdamtopG <120588189+AdamtopG@users.noreply.github.com>
2025-04-26 17:35:24 +0200

    Debug du code databaseconnection

--- util/Databaseconnection.java
+++ util/Databaseconnection.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Databaseconnection {
-    // URL de connexion (adaptée à votre configuration, ici sur localhost et port 3306)
+    // URL de connexion (adapte a notre configuration ici sur localhost et port 3306)
     private static final String URL = "jdbc:mysql://localhost:8889/ShoppingDB";
-    private static final String USER = "root";        // Remplacez par votre utilisateur MySQL
-    private static final String PASSWORD = "root";      // Remplacez par votre mot de passe MySQL
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
             // Chargement du driver JDBC MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Établissement de la connexion
+            // etablir la connexion
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connexion à la base réussie !");
         } catch (ClassNotFoundException e) {
